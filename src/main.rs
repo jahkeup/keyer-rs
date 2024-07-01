1bdbb64dce16396881eba69c92ea43b79552ed78
Jacob Vallejo <jake@jahkeup.com>
2024-06-30 17:08:19 -0700

    feat: setup and teardown nicely

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::{io::Write, thread, time::Duration};
 
 use log::info;
 use serialport;
@@ -22,16 +22,40 @@ fn main() -> Result<(), Error> {
     let builder = serialport::new(port_path, 1200);
     dbg!(&builder);
 
+    info!("connecting...");
+
     let mut serial_port = builder.open_native()?;
     dbg!(&serial_port);
+
+    info!("connected to serial device");
+    info!("starting session");
 
     let open_command = Command::Admin(AdminCommand::OpenHostConnection);
     let cmd: Vec<u8> = open_command.try_into().expect("open command bytes");
     serial_port.write(&cmd).expect("write");
 
-    let key_command = Command::DoKey(KeyInput::Dah);
-    let cmd: Vec<u8> = key_command.try_into().expect("key command bytes");
-    serial_port.write(&cmd).expect("write");
+    for _ in 1..=5 {
+
+        let key_command = Command::DoKey(KeyInput::Dah);
+        let cmd: Vec<u8> = key_command.try_into().expect("key command bytes");
+        serial_port.write(&cmd).expect("write");
+
+
+        thread::sleep(Duration::from_secs(3));
+
+        let key_command = Command::DoKey(KeyInput::Release);
+        let cmd: Vec<u8> = key_command.try_into().expect("key command bytes");
+        serial_port.write(&cmd).expect("write");
+
+        thread::sleep(Duration::from_secs(1));
+
+    }
+
+    let close_command = Command::Admin(AdminCommand::CloseHostConnection);
+    let cmd: Vec<u8> = close_command.try_into().expect("close command bytes");
+    serial_port.write(&cmd).expect("write close");
+
+    info!("closed session");
 
     Ok(())
 }
